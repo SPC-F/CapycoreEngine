7d43286f6fa72fa3e6c6cc0e7ffbb3f1f293df88
Luka <60887692+BotSquirrel@users.noreply.github.com>
2025-10-27 13:17:20 +0100

    feat (profiling): add tracy profiler client and memory leak detection (#21)
    * feat (profiling): add tracy profiler client and memory leak detection
    
    * fix (cmake): add option to disable tracy
    
    * fix (cmake): exclude tracy from tests
    
    * fix (profiler): macro's for tracy

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <engine/core/engine.h>
+#include <engine/util/memory.h>
+
+// Macro's for tracy based on the CMake option
+#ifdef TRACY_ENABLE
+#define TRACY_INIT() tracy_memory_init()
+#define TRACY_SHUTDOWN() tracy_memory_shutdown()
+#else
+#define TRACY_INIT()
+#define TRACY_SHUTDOWN()
+#endif
 
 int main() {
+    TRACY_INIT();
+
     Engine engine;
+
+    TRACY_SHUTDOWN();
     return 0;
 }
