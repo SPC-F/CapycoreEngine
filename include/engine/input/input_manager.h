dd9cff77b110069a69bc3cdb1a69c4fe770c81f6
Lars Devans <l.beijaard1@student.avans.nl>
2025-10-28 11:40:43 +0100

    feat(input): add skeletons

---
+++ include/engine/input/input_manager.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "engine/input/i_input_provider.h"
+
+/**
+ * @brief Centralized input manager that delegates to an external input provider.
+ */
+class InputManager {
+public:
+    IInputProvider* provider() const noexcept;
+    void set_provider(IInputProvider* provider) noexcept;
+
+private:
+    IInputProvider* _provider = nullptr;
+};
