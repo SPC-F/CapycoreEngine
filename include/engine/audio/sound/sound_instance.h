c36a809620ebb16bf13840783cc81f19a337a5d4
Luka Leppens <60887692+BotSquirrel@users.noreply.github.com>
2025-10-30 14:01:12 +0100

    fix (sound): add functions to sound as to seperate SDL func's

--- include/engine/audio/sound/sound_instance.h
+++ include/engine/audio/sound/sound_instance.h
@@ -6,10 +6,19 @@
 /**
  * @brief Represents an instance of a generic sound within the audio system.
  */
-struct SoundInstance {
-    SoundInstance() = default;
+class SoundInstance {
+public:
+    SoundInstance(const std::string& name, const std::string& file_path, float volume = 1.0f)
+        : name(name), file_path(file_path), volume(volume) {}
     virtual ~SoundInstance() = default;
 
+    virtual void play() = 0;
+    virtual void pause() = 0;
+    virtual void stop() = 0;
+    virtual void setVolume(float volume) = 0;
+    virtual bool isPlaying() const = 0;
+
+protected:
     std::string name;
     std::string file_path;
 
