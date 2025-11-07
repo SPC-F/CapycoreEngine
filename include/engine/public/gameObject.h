346b2a2a0b685aae38cd09b40fd72b4e35122ef2
Luka Leppens <60887692+BotSquirrel@users.noreply.github.com>
2025-11-07 12:53:50 +0100

    fix (linter): preset and build target

--- include/engine/public/gameObject.h
+++ include/engine/public/gameObject.h
@@ -29,7 +29,7 @@ public:
     virtual ~GameObject();
 
     GameObject& parent(GameObject& parent);
-    GameObject& parent(std::nullopt_t);
+    GameObject& parent(std::nullopt_t nullopt);
 
     [[nodiscard]] bool is_active_in_world() const noexcept;
     [[nodiscard]] bool is_active() const noexcept;
