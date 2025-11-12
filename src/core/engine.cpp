b40de4f5709fa2bf79948ba6d82b01f3ce7acd4a
Thomas Ruijters <tpj.ruijters@student.avans.nl>
2025-11-12 23:02:57 +0100

    fix: Make the engine destroyable before the loop ends. (#70)

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -1,6 +1,23 @@
 #include <engine/core/engine.h>
 #include <engine/audio/audio_service.h>
+#include <SDL3/SDL.h>
 
 Engine::Engine() : services(std::make_unique<ServiceContainer>()) {
     services->register_service<AudioService>();
 }
+
+Engine& Engine::instance() {
+    if (!engine_instance_) {
+        SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+        engine_instance_ = std::unique_ptr<Engine, Deleter>(new Engine());
+    }
+
+    return *engine_instance_;
+}
+
+void Engine::quit() {
+    engine_instance_.reset();
+    SDL_Quit();
+}
+
+std::unique_ptr<Engine, Engine::Deleter> Engine::engine_instance_ = nullptr;
