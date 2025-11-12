a8e379492c0d0b4957f72bb822056c9d42773945
Thomas Ruijters <thomas.ruijters@gmail.com>
2025-11-12 11:28:51 +0100

    feat: Register rendering service

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -1,9 +1,11 @@
 #include <engine/core/engine.h>
 #include <engine/audio/audio_service.h>
 #include <engine/core/assetManager.h>
+#include <engine/core/rendering/renderingService.h>
 
 Engine::Engine() :
     services(std::make_unique<ServiceContainer>()) {
     services->register_service<AssetManager>();
+    services->register_service<RenderingService>();
     services->register_service<AudioService>();
 }
