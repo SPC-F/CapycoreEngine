f52b173df811250698c6e9dcd1e81624961b9719
Thomas Ruijters <thomas.ruijters@gmail.com>
2025-11-12 10:43:28 +0100

    fix: Make the engine destroyable before the loop ends.
    Due to a goof with static memory and singletons not being nice, we implemented a workaround that should manage the singleton from the instance, not the static class alone.

--- include/engine/core/engine.h
+++ include/engine/core/engine.h
@@ -1,16 +1,22 @@
 #pragma once
+
 #include <memory>
+#include <iostream>
 
 #include <engine/core/serviceContainer.h>
 
 class Engine {
 private:
+    struct Deleter {
+        void operator()(Engine* e) const { delete e; }
+    };
+
     Engine();
+
+    static std::unique_ptr<Engine, Deleter> engine_instance;
 public:
-    static Engine& instance() {
-        static Engine engine {};
-        return engine;
-    }
+    static Engine& instance();
+    static void quit();
 
     const std::unique_ptr<ServiceContainer> services;
 };
--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -1,6 +1,23 @@
 #include <engine/core/engine.h>
 #include <engine/audio/audio_service.h>
+#include <SDL3/SDL.h>
 
 Engine::Engine() : services(std::make_unique<ServiceContainer>()) {
     services->register_service<AudioService>();
 }
+
+Engine& Engine::instance() {
+    if (!engine_instance) {
+        SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+        engine_instance = std::unique_ptr<Engine, Deleter>(new Engine());
+    }
+
+    return *engine_instance;
+}
+
+void Engine::quit() {
+    engine_instance.reset();
+    SDL_Quit();
+}
+
+std::unique_ptr<Engine, Engine::Deleter> Engine::engine_instance = nullptr;
