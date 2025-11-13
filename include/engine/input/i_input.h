b8f2c35c2998436663f6d67e34d99801cdad7852
Lars Devans <l.beijaard1@student.avans.nl>
2025-11-13 21:25:02 +0100

    docs(input): add method comments

--- include/engine/input/i_input.h
+++ include/engine/input/i_input.h
@@ -10,5 +10,13 @@ class IInput {
 public:
     virtual ~IInput() = default;
 
+    /**
+    * @brief Updates the state of keyboard and mouse inputs.
+    *
+    * This function is responsible for querying the current input states, including
+    * key presses, key releases, mouse button presses, releases, and mouse movement.
+    * It updates the provided `key_states` and `mouse_state` with the latest input
+    * information for the current frame.
+    */
     virtual void update(std::map<KeyCode, KeyState>& key_states, MouseState& mouse_state);
 };
