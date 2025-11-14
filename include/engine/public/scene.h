eef9c4f2126ad0c2dcbd98a3ad54506641e6078f
Thomas Ruijters <thomas.ruijters@gmail.com>
2025-11-14 18:55:51 +0100

    feat: Scene skeleton

--- include/engine/public/scene.h
+++ include/engine/public/scene.h
@@ -1,5 +1,26 @@
 #pragma once
 
-class Scene {
+#include <vector>
+#include <memory>
+#include <engine/public/gameObject.h>
 
+class Scene {
+private:
+    bool is_running_;
+    float time_modifier_;
+    const std::vector<std::unique_ptr<GameObject>> game_objects_;
+
+public:
+    Scene(const std::string& name);
+    virtual ~Scene();
+
+    void on_awake();
+    void destroy();
+    void run();
+
+    Scene& time_modifier(float modifier);
+    float time_modifier() const;
+
+    const std::vector<std::reference_wrapper<GameObject>> game_objects() const;
+    Scene& add_game_object(std::unique_ptr<GameObject> game_object);
 };
