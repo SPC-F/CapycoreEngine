35eb0c5016bc96f3992716510a093fadd9b83411
Thomas Ruijters <thomas.ruijters@gmail.com>
2025-11-16 23:01:25 +0100

    fix: naming

--- tests/engine/public/GameObjectTests.cpp
+++ tests/engine/public/GameObjectTests.cpp
@@ -3,7 +3,7 @@
 #include <set>
 
 #include <catch2/catch_test_macros.hpp>
-#include <engine/public/gameobject.h>
+#include <engine/public/gameObject.h>
 #include <engine/public/scene.h>
 
 TEST_CASE("GameObject default constructor initializes correctly", "[GameObject]") {
