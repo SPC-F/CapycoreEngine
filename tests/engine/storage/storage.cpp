cde80574e3e89983d034a3ba65bb7c913101580d
LarsDevans <l.beijaard1@student.avans.nl>
2025-11-19 10:04:01 +0100

    test(storage): add save and load

--- tests/engine/storage/storage.cpp
+++ tests/engine/storage/storage.cpp
@@ -1,11 +1,22 @@
 #include <catch2/catch_approx.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <filesystem>
+#include <fstream>
 
 #include <engine/storage/simple_storage.h>
 
 class TestableStorage : public SimpleStorage {
 public:
     using SimpleStorage::session_storage_;
+
+    static TestableStorage & instance()
+    {
+        static TestableStorage inst;
+        return inst;
+    }
+
+    void test_load() { load(); }
+    void test_save() { save(); }
 };
 
 TEST_CASE("try_get_returns_empty_optional_when_key_missing", "[SimpleStorage]")
@@ -80,3 +91,116 @@ TEST_CASE("delete_all_removes_every_entry", "[SimpleStorage]")
     REQUIRE(storage.try_get("x").has_value() == false);
     REQUIRE(storage.try_get("y").has_value() == false);
 }
+
+TEST_CASE("save_writes_file_and_load_restores_int_float_string", "[SimpleStorage][Persistence]")
+{
+    auto& storage = TestableStorage::instance();
+    storage.delete_all();
+
+    storage.set_value("i", 123);
+    storage.set_value("f", 4.5f);
+    storage.set_value("s", std::string("hello world"));
+
+    storage.save();
+
+    storage.delete_all();
+    storage.test_load();
+
+    REQUIRE(storage.try_get("i").has_value());
+    REQUIRE(std::get<int>(*storage.try_get("i")) == 123);
+
+    REQUIRE(storage.try_get("f").has_value());
+    REQUIRE(std::get<float>(*storage.try_get("f")) == Catch::Approx(4.5f));
+
+    REQUIRE(storage.try_get("s").has_value());
+    REQUIRE(std::get<std::string>(*storage.try_get("s")) == "hello world");
+}
+
+TEST_CASE("save_then_load_preserves_overwritten_values", "[SimpleStorage][Persistence]")
+{
+    auto& storage = TestableStorage::instance();
+    storage.delete_all();
+
+    storage.set_value("a", 1);
+    storage.set_value("a", std::string("updated"));
+    storage.set_value("b", 3.14f);
+
+    storage.save();
+
+    storage.delete_all();
+    storage.test_load();
+
+    REQUIRE(storage.try_get("a").has_value());
+    REQUIRE(std::get<std::string>(*storage.try_get("a")) == "updated");
+
+    REQUIRE(storage.try_get("b").has_value());
+    REQUIRE(std::get<float>(*storage.try_get("b")) == Catch::Approx(3.14f));
+}
+
+TEST_CASE("load_ignores_malformed_lines", "[SimpleStorage][Persistence]")
+{
+    auto& storage = TestableStorage::instance();
+    storage.delete_all();
+
+    {
+        std::filesystem::create_directories("saved");
+        std::ofstream out("saved/simple.dat", std::ios::trunc);
+        out << "good=42\n";
+        out << "bad_line_without_equals\n"; // Malformed line
+        out << "also_bad=\n";
+        out << "str=\"abc\"\n";
+        out << "float=1.25\n";
+    }
+
+    storage.test_load();
+
+    REQUIRE(storage.try_get("good").has_value());
+    REQUIRE(std::get<int>(*storage.try_get("good")) == 42);
+
+    REQUIRE(storage.try_get("str").has_value());
+    REQUIRE(std::get<std::string>(*storage.try_get("str")) == "abc");
+
+    REQUIRE(storage.try_get("float").has_value());
+    REQUIRE(std::get<float>(*storage.try_get("float")) == Catch::Approx(1.25f));
+
+    REQUIRE(storage.try_get("bad_line_without_equals").has_value() == false);
+    REQUIRE(storage.try_get("also_bad").has_value() == false);
+}
+
+TEST_CASE("load_handles_escaped_strings", "[SimpleStorage][Persistence]")
+{
+    auto& storage = TestableStorage::instance();
+    storage.delete_all();
+
+    {
+        std::filesystem::create_directories("saved");
+        std::ofstream out("saved/simple.dat", std::ios::trunc);
+        out << "escaped=\"hello \\\"world\\\"\"\n";
+    }
+
+    storage.test_load();
+
+    REQUIRE(storage.try_get("escaped").has_value());
+    REQUIRE(std::get<std::string>(*storage.try_get("escaped")) == "hello \"world\"");
+}
+
+TEST_CASE("save_overwrites_previous_file", "[SimpleStorage][Persistence]")
+{
+    auto& storage = TestableStorage::instance();
+    storage.delete_all();
+
+    storage.set_value("old", 1);
+    storage.save();
+
+    storage.delete_all();
+    storage.set_value("new", 999);
+    storage.save();
+
+    storage.delete_all();
+    storage.test_load();
+
+    REQUIRE(storage.try_get("old").has_value() == false);
+    REQUIRE(storage.try_get("new").has_value());
+    REQUIRE(std::get<int>(*storage.try_get("new")) == 999);
+}
+
