477b76f089088533ff1c59db865f29b34767ab7f
Luka Leppens <60887692+BotSquirrel@users.noreply.github.com>
2025-11-20 16:01:01 +0100

    fix (physics): broken test remnant from development

--- tests/engine/physics/creation.cpp
+++ tests/engine/physics/creation.cpp
@@ -6,7 +6,6 @@
 #include <engine/public/component.h>
 #include <engine/public/gameObject.h>
 #include <engine/public/util/vector3.h>
-#include <engine/public/scene.h>
 #include <engine/public/scene_service.h>
 
 struct DummyGameObject : public GameObject {
@@ -29,7 +28,10 @@ TEST_CASE("physics_creation_factory_creates_body", "[PhysicsCreationFactory]") {
 
     Vector3 position{0.0f, 0.0f, 0.0f};
 
-    DummyScene dummy_scene;
+    const std::string& scene_name = "Test Scene";
+    auto scene_service = SceneService();
+    Scene& dummy_scene = scene_service.add_scene(scene_name);
+    
     DummyGameObject dummy_game_object(dummy_scene);
     DummyComponent dummy_component;
     auto& comp = dummy_component.parent(dummy_game_object);
