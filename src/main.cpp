67a7e109e2534e0ce3b930b7a32ad114cc5cf59c
SjulGr <sjulgrooten@live.nl>
2025-11-21 23:18:56 +0100

    refactor(main): remove test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include <engine/util/memory.h>
 #include <engine/core/engine.h>
-#include <engine/network/multiplayer_service.h>
-
-#include <iostream>
 
 // Macro's for tracy based on the CMake option
 namespace {
@@ -17,35 +14,6 @@ namespace {
 
 int main(int argc, char* argv[]) {
     tracy_init();
-
-    try {
-        auto& engine = Engine::instance();
-        auto& multiplayer_service = engine.services->get_service<MultiplayerService>().get();
-
-        multiplayer_service.set_connection_port(1024);
-
-        if (strcmp(argv[1], "h") == 0) {
-            multiplayer_service.set_host();
-            multiplayer_service.set_max_clients(2);
-            multiplayer_service.start_server();
-        } else {
-            multiplayer_service.set_client();
-            multiplayer_service.connect("127.0.0.1");
-        }
-
-        while (multiplayer_service.get_connection_state() != ConnectionState::DISCONNECTED)
-        {
-            if (strcmp(argv[1], "h") == 0 && multiplayer_service.get_client_amount() >= 2)
-                multiplayer_service.disconnect();
-            else if (multiplayer_service.get_connection_state() == ConnectionState::DISCONNECTED)
-                break;
-            multiplayer_service.poll();
-        }
-    } catch (std::string err) {
-        std::cout << err << std::endl;
-    }
-
-    Engine::quit();
 
     tracy_shutdown();
     return 0;
