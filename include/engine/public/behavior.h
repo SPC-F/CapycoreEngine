2c8d32a685aa5c112c40fe75f39fe478a13d1cea
Luka <60887692+BotSquirrel@users.noreply.github.com>
2025-11-21 14:21:49 +0100

    feat (behavior): add extendable class and component (#103)
    * feat (behavior): add overridable behavior class
    
    * fix (component): add hooks
    
    * feat (behavior): add script component
    
    * feat (behavior): add tests and merge fixes
    
    * feat (behavior): move to right location and doc comments
    
    * fix (behavior): relocation of file
    
    * fix (behavior): add specifc methods for enable/disable
    
    * fix (gameobject): assign value to nodiscard
    
    * fix (behavior): started check for first frame
    
    * fix (behavior): default implementations and add no lint for parents
    
    * fix (behavior): set mark for deletion on gameobject
    
    * fix (gameobject): remove value assignment for nodiscard

---
+++ include/engine/public/behavior.h
@@ -0,0 +1,133 @@
+#pragma once
+
+#include <optional>
+#include <functional>
+#include <stdexcept>
+
+#include <engine/public/gameObject.h>
+#include <engine/public/component.h>
+
+/**
+ * @brief Base class for defining behaviors that can be attached to Components.
+ * @note This class is inteded to be inherited from to create specific behaviors.
+ * 
+ * The Behavior class provides a structured way to define lifecycle methods
+ * for game logic, such as initialization, updates, and destruction.
+ * It also offers utility methods to interact with the associated GameObject
+ * and its Components.
+ */
+class Behavior
+{
+public:
+    Behavior();
+    virtual ~Behavior() = default;
+
+    void attach(Component& component);
+
+    virtual void on_awake() {};
+    virtual void on_start() {};
+    virtual void on_update(float dt) = 0;
+    virtual void on_destroy() {};
+
+    [[nodiscard]] GameObject& game_object() const;
+    [[nodiscard]] Transform& transform() const;
+
+    [[nodiscard]] bool enabled() const;
+    Behavior& enable();
+    Behavior& disable();
+
+    template<typename T>
+    std::optional<std::reference_wrapper<T>> get_component() 
+    {
+        if (!attached_component_) {
+            throw std::runtime_error("Behavior has no associated GameObject.");
+        }
+
+        return game_object().template get_component<T>();
+    }
+
+    template<typename T>
+    std::vector<std::reference_wrapper<T>> get_components() 
+    {
+        if (!attached_component_) {
+            throw std::runtime_error("Behavior has no associated GameObject.");
+        }
+
+        return game_object().template get_components<T>();
+    }
+
+    template<typename T>
+    std::optional<std::reference_wrapper<T>> get_component_from_children() 
+    {
+        if (!attached_component_) {
+            throw std::runtime_error("Behavior has no associated GameObject.");
+        }
+
+        for (auto& child : game_object().children()) {
+            auto comp = child.get().template get_component<T>();
+            if (comp) {
+                return comp;
+            }
+        }
+
+        return std::nullopt;
+    }
+
+    template<typename T>
+    std::vector<std::reference_wrapper<T>> get_components_from_children() 
+    {
+        if (!attached_component_) {
+            throw std::runtime_error("Behavior has no associated GameObject.");
+        }
+
+        return game_object().template get_components_from_children<T>();
+    }
+
+    template<typename T>
+    std::optional<std::reference_wrapper<T>> get_component_in_parent() 
+    {
+        if (!attached_component_) {
+            throw std::runtime_error("Behavior has no associated GameObject.");
+        }
+
+        auto current_parent = game_object().parent();
+
+        while (current_parent) {
+            auto comp = current_parent->get().template get_component<T>();
+            if (comp) {
+                return comp;
+            }
+            current_parent = current_parent->get().parent();
+        }
+
+        return std::nullopt;
+    }
+
+    template<typename T>
+    std::vector<std::reference_wrapper<T>> get_components_in_parent()
+    {
+        if (!attached_component_) {
+            throw std::runtime_error("Behavior has no associated GameObject.");
+        }
+
+        std::vector<std::reference_wrapper<T>> result;
+        auto current_parent = game_object().parent();
+
+        while (current_parent) {
+            auto comps = current_parent->get().template get_components<T>();
+            result.insert(result.end(), comps.begin(), comps.end());
+            
+            current_parent = current_parent->get().parent();
+        }
+
+        return result;
+    }
+
+    void destroy();
+    void destroy(Component& component);
+    void destroy(GameObject& game_object);
+
+private:
+    std::optional<std::reference_wrapper<Component>> attached_component_;
+    bool enabled_ {true};
+};
---
+++ tests/engine/core/behavior.cpp
@@ -0,0 +1,116 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <engine/public/behavior.h>
+#include <engine/public/scene.h>
+#include <engine/public/component.h>
+#include <engine/public/components/behaviorscript.h>
+
+TEST_CASE("behavior_runs_on_awake", "[Behavior]")
+{
+    class TestBehavior : public Behavior {
+    public:
+        void on_awake() override {
+            awake_called = true;
+        }
+        void on_start() override {}
+        void on_update(float dt) override {}
+        void on_destroy() override {}
+
+        bool awake_called = false;
+    };
+
+    Scene scene;
+    GameObject game_object(scene);
+    auto& behavior_script = game_object.add_component<BehaviorScript>(std::make_unique<TestBehavior>());
+
+    REQUIRE(behavior_script.enabled() == true);
+    REQUIRE(dynamic_cast<TestBehavior*>(&behavior_script.behavior())->awake_called == true);
+}
+
+TEST_CASE("behavior_on_start_and_update", "[Behavior]")
+{
+    class TestBehavior : public Behavior {
+    public:
+        void on_awake() override {}
+        void on_start() override {
+            start_called = true;
+        }
+        void on_update(float dt) override {
+            update_called = true;
+        }
+        void on_destroy() override {}
+
+        bool start_called = false;
+        bool update_called = false;
+    };
+
+    Scene scene;
+    GameObject game_object(scene);
+    auto& behavior_script = game_object.add_component<BehaviorScript>(std::make_unique<TestBehavior>());
+
+    constexpr float dt = 0.016f;
+
+    behavior_script.update(dt);
+    REQUIRE(dynamic_cast<TestBehavior*>(&behavior_script.behavior())->start_called == true);
+    REQUIRE(dynamic_cast<TestBehavior*>(&behavior_script.behavior())->update_called == false);
+
+    behavior_script.update(dt);
+    REQUIRE(dynamic_cast<TestBehavior*>(&behavior_script.behavior())->update_called == true);
+}
+
+TEST_CASE("behavior_on_destroy_called_on_destruction", "[Behavior]")
+{
+    bool destroy_called = false;
+    auto callback = [&destroy_called](Behavior& behavior) {
+        destroy_called = true;
+    };
+
+    class TestBehavior : public Behavior {
+    public:
+        using callback_t = std::function<void(Behavior&)>;
+        callback_t on_destroy_callback_;
+
+        TestBehavior(std::function<void(Behavior&)> on_destroy_callback)
+            : on_destroy_callback_(on_destroy_callback) {}
+
+        void on_awake() override {}
+        void on_start() override {}
+        void on_update(float dt) override {}
+        void on_destroy() override {
+            on_destroy_callback_(*this);
+        }
+    };
+
+    {
+        Scene scene;
+        GameObject game_object(scene);
+        auto& behavior_script = game_object.add_component<BehaviorScript>(
+            std::make_unique<TestBehavior>(callback)
+        );
+
+        REQUIRE(behavior_script.enabled() == true);
+        game_object.remove_component(behavior_script);
+    }
+
+    REQUIRE(destroy_called == true);
+}
+
+TEST_CASE("behavior_can_access_game_object_and_transform", "[Behavior]")
+{
+    class TestBehavior : public Behavior {
+    public:
+        void on_awake() override {}
+        void on_start() override {}
+        void on_update(float dt) override {}
+        void on_destroy() override {}
+    };
+
+    auto behavior = std::make_unique<TestBehavior>();
+
+    Scene scene;
+    GameObject game_object(scene);
+    auto& behavior_script = game_object.add_component<BehaviorScript>(std::make_unique<TestBehavior>());
+    
+    REQUIRE(dynamic_cast<TestBehavior*>(&behavior_script.behavior())->game_object().id() == game_object.id());
+    REQUIRE(&dynamic_cast<TestBehavior*>(&behavior_script.behavior())->transform() == &game_object.transform());
+}
