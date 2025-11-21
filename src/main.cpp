71a73749b4b8bbeca4a3fcc85e36c2e02ed821dd
Luka Leppens <60887692+BotSquirrel@users.noreply.github.com>
2025-11-21 14:16:29 +0100

    cleanup (main): remove test code

--- src/main.cpp
+++ src/main.cpp
@@ -11,35 +11,8 @@ namespace {
     #endif
 }
 
-#include <engine/core/engine.h>
-#include <engine/public/scene_service.h>
-#include <engine/public/gameObject.h>
-#include <engine/public/components/sprite.h>
-#include <engine/core/rendering/assetService.h>
-#include <engine/core/rendering/renderingService.h>
-
-void yes() {
-    auto& engine = Engine::instance();
-    auto& scene_service = engine.services->get_service<SceneService>().get();
-    auto& asset_service = engine.services->get_service<AssetService>().get();
-    auto& rendering_service = engine.services->get_service<RenderingService>().get();
-
-    asset_service.load_from_resource("Fred_Sprites_REDO.png", "FRED", 12, 6);
-    asset_service.register_texture("FRED", "TestTexture", 0);
-
-    auto& scene = scene_service.add_scene("SomeScene");
-    GameObject& game_object = scene.add_game_object("SomeGameObject");
-    game_object.transform().scale({3, 3, 1});
-    Sprite& sprite = game_object.add_component<Sprite>("TestTexture", Color(), 0, 0, 1, 1);
-
-    scene_service.load_scene("SomeScene");
-
-    Engine::quit();
-}
-
 int main() {
     tracy_init();
-    yes();
     tracy_shutdown();
     return 0;
 }
