3f7b197866fa99681bd01614b8c87a539365545c
Luka Leppens <60887692+BotSquirrel@users.noreply.github.com>
2025-11-28 12:23:23 +0100

    fix (sprite): texture width and height

--- src/core/rendering/strategies/sdl/sdl_sprite_strategy.cpp
+++ src/core/rendering/strategies/sdl/sdl_sprite_strategy.cpp
@@ -43,6 +43,11 @@ void SdlSpriteStrategy::draw(Component& component) {
     float width = default_texture_width;
     float height = default_texture_height;
 
+    if (texture_ptr != nullptr) {
+        width = static_cast<float>(texture_ptr->w);
+        height = static_cast<float>(texture_ptr->h);
+    }
+
     auto const source = SDL_FRect {
         .x = 0,
         .y = 0,
