42e14172feea729b3ec01a78189c9301dfa320b8
Thomas Ruijters <thomas.ruijters@gmail.com>
2025-11-30 14:53:44 +0100

    fix: remove test function from main

--- src/main.cpp
+++ src/main.cpp
@@ -11,45 +11,9 @@ namespace {
     #endif
 }
 
-#include <engine/core/engine.h>
-#include <engine/public/scene_service.h>
-#include <engine/public/gameObject.h>
-#include <engine/public/components/sprite.h>
-#include <engine/core/rendering/assetService.h>
-#include <engine/core/rendering/renderingService.h>
-
-void yes() {
-    const auto& engine = Engine::instance();
-    auto& scene_service = engine.services->get_service<SceneService>().get();
-    auto& asset_service = engine.services->get_service<AssetService>().get();
-    auto& rendering_service = engine.services->get_service<RenderingService>().get();
-
-    asset_service.load_from_resource("Fred_Sprites_REDO.png", "FRED", 12, 6);
-    asset_service.register_texture("FRED", "TestTexture", 0);
-    asset_service.register_texture("FRED", "TestTexture2", 5);
-
-    auto& scene = scene_service.add_scene("SomeScene");
-    scene.add_game_object<Camera>(scene, Color(), 1.0f, true);
-
-    GameObject& game_object = scene.add_game_object("SomeGameObject");
-    game_object.transform().scale({3, 3, 1});
-    Sprite& sprite = game_object.add_component<Sprite>("TestTexture", Color(), 0, 0, 1, 1);
-    sprite.rendering_layer(3);
-
-    GameObject& game_object2 = scene.add_game_object("SomeGameObject");
-    game_object2.transform().position({19, 11, 1});
-    game_object2.transform().scale({3, 3, 1});
-    Sprite& sprite2 = game_object2.add_component<Sprite>("TestTexture", Color(), 0, 0, 1, 1);
-    sprite2.rendering_layer(5);
-
-    scene_service.load_scene("SomeScene");
-
-    Engine::quit();
-}
-
 int main() {
     tracy_init();
-    yes();
+
     tracy_shutdown();
     return 0;
 }
