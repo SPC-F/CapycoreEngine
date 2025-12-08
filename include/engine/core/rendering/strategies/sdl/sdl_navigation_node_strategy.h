13e12da6bd2c49930d606df78d384c09d8ed30a2
Luka Leppens <60887692+BotSquirrel@users.noreply.github.com>
2025-12-08 18:33:28 +0100

    fix (renderable): navigation node comments

--- include/engine/core/rendering/strategies/sdl/sdl_navigation_node_strategy.h
+++ include/engine/core/rendering/strategies/sdl/sdl_navigation_node_strategy.h
@@ -6,12 +6,12 @@
 #include <engine/public/util/color.h>
 
 /**
- * @brief SDL implementation of the box collider 2D rendering strategy.
+ * @brief SDL implementation of the navigation node rendering strategy.
  *
- * SdlBoxCollider2DStrategy is responsible for rendering BoxCollider2D
+ * SdlNavigationNodeStrategy is responsible for rendering NavigationNode
  * components using SDL. It utilizes an SDL_Renderer to draw the associated
- * Texture of the BoxCollider2D component onto the screen, applying
- * transformations and color modulations as specified by the BoxCollider2D.
+ * Texture of the NavigationNode component onto the screen, applying
+ * transformations and color modulations as specified by the NavigationNode.
  */
 class SdlNavigationNodeStrategy final : public IRenderingStrategy {
  private:
